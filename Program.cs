ec3d15b3749ed7f0142a3d5fb281d112bd0d4d8c
leleol <e107ti@cps.sp.gov.br>
2022-02-18 22:43:00 -0300

    oi

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Etec Adolpho Berezin");
 Console.WriteLine("Olá usuário, eu me chamo JUJU!");
+Console.WriteLine("oiiiiiiiiiii");
